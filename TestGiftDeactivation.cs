aa0c786812c8f0a5da9667b4b08d8feb082fe79a
Moneris Solutions <samar.soubra@gmail.com>
2017-04-27 08:55:16 -0400

    Add files via upload

---
+++ TestGiftDeactivation.cs
@@ -0,0 +1,101 @@
+namespace Moneris
+{
+    using System;
+
+	public class TestGiftDeactivation
+	{
+	  public static void Main(string[] args)
+	  {
+	        string host = "esqa.moneris.com";
+            string store_id = args[0];
+            string api_token = args[1];
+	        string order_id = args[2];
+            //Console.WriteLine("Please swipe card");
+            //track1 = Console.ReadLine();
+            //track2 = Console.ReadLine();
+            string track2 = "";
+            
+            string info = args[3];
+            string pan = args[4];
+            string expdate = args[5];
+            string language_code = args[6];
+            string cvd_value = args[7];
+
+            ErnexGiftDeactivation ernexGiftDeactivation = new ErnexGiftDeactivation(order_id, track2, pan, expdate, language_code);
+
+            ernexGiftDeactivation.SetCvdValue(cvd_value); //optional
+            ernexGiftDeactivation.SetInfo(info); //optional
+
+	        ErnexHttpsPostRequest mpgReq =
+	            new ErnexHttpsPostRequest(host, store_id, api_token, ernexGiftDeactivation);
+	        try
+	        {
+	            ErnexReceipt receipt = mpgReq.GetReceipt();
+
+                Console.WriteLine("ReceiptId = " + receipt.GetReceiptId());
+                Console.WriteLine("ResponseCode = " + receipt.GetResponseCode());
+                Console.WriteLine("HostReferenceNum = " + receipt.GetHostReferenceNum());
+                Console.WriteLine("TransTime = " + receipt.GetTransTime());
+                Console.WriteLine("TransDate = " + receipt.GetTransDate());
+                Console.WriteLine("TransType = " + receipt.GetTransType());
+                Console.WriteLine("Message = " + receipt.GetMessage());
+                Console.WriteLine("TransCardCode = " + receipt.GetTransCardCode());
+                Console.WriteLine("TransCardType = " + receipt.GetTransCardType());
+                Console.WriteLine("TxnNumber = " + receipt.GetTxnNumber());
+                Console.WriteLine("TimedOut = " + receipt.GetTimedOut());
+                Console.WriteLine("HostTotals = " + receipt.GetHostTotals());
+                Console.WriteLine("DisplayText = " + receipt.GetDisplayText());
+                Console.WriteLine("ReceiptText = " + receipt.GetReceiptText());
+                Console.WriteLine("CardHolderName = " + receipt.GetCardHolderName());
+                Console.WriteLine("VoucherType = " + receipt.GetVoucherType());
+                Console.WriteLine("VoucherText = " + receipt.GetVoucherText());
+                Console.WriteLine("InitialAmount = " + receipt.GetInitialAmount());
+                Console.WriteLine("InitialBalance = " + receipt.GetInitialBalance());
+                Console.WriteLine("BatchNo = " + receipt.GetBatchNo());
+                Console.WriteLine("CurrentBalance = " + receipt.GetCurrentBalance());
+                Console.WriteLine("Benefit = " + receipt.GetBenefit());
+                Console.WriteLine("Language = " + receipt.GetLanguage());
+                Console.WriteLine("Error Code = " + receipt.GetErrorCode());
+                Console.WriteLine("Error Message = " + receipt.GetErrorMessage());
+                Console.WriteLine("ActivationCharge = " + receipt.GetActivationCharge());
+                Console.WriteLine("RemainingBalance = " + receipt.GetRemainingBalance());
+                Console.WriteLine("CardStatus = " + receipt.GetCardStatus());
+
+                foreach (string cardCode in receipt.GetCardCodes())
+                {
+                    Console.WriteLine("cardCode = " + cardCode);
+                    Console.WriteLine("CardCardType = " + receipt.GetCardCardType(cardCode));
+                    Console.WriteLine("CheckMod10 = " + receipt.GetCheckMod10(cardCode));
+                    Console.WriteLine("CheckLanguage = " + receipt.GetCheckLanguage(cardCode));
+                    Console.WriteLine("CVCPrompt = " + receipt.GetCVCPrompt(cardCode));
+                    Console.WriteLine("InfoPrompt = " + receipt.GetInfoPrompt(cardCode));
+                    Console.WriteLine("InitialAmountPrompt = " + receipt.GetInitialAmountPrompt(cardCode));
+                    Console.WriteLine("RefundAllowed = " + receipt.GetRefundAllowed(cardCode));
+                    Console.WriteLine("CardLengthMinimum = " + receipt.GetCardLengthMinimum(cardCode));
+                    Console.WriteLine("CardLengthMaximum = " + receipt.GetCardLengthMaximum(cardCode));
+                    Console.WriteLine("LowBIN1 = " + receipt.GetLowBIN1(cardCode));
+                    Console.WriteLine("HighBIN1 = " + receipt.GetHighBIN1(cardCode));
+                    Console.WriteLine("LowBIN2 = " + receipt.GetLowBIN2(cardCode));
+                    Console.WriteLine("HighBIN2 = " + receipt.GetHighBIN2(cardCode));
+                    Console.WriteLine("LowBIN3 = " + receipt.GetLowBIN3(cardCode));
+                    Console.WriteLine("HighBIN3 = " + receipt.GetHighBIN3(cardCode));
+                    Console.WriteLine("LowBIN4 = " + receipt.GetLowBIN4(cardCode));
+                    Console.WriteLine("HighBIN4 = " + receipt.GetHighBIN4(cardCode) + "\n");
+
+                    foreach (string recordType in receipt.GetRecordType(cardCode))
+                    {
+                        Console.WriteLine("recordType = " + recordType);
+                        Console.WriteLine("CardDescription = " + receipt.GetCardDescription(cardCode, recordType));
+                        Console.WriteLine("InfoPromptText = " + receipt.GetInfoPromptText(cardCode, recordType));
+                    }
+
+                }
+            }
+	        catch (Exception e)
+	        {
+	            Console.WriteLine(e);
+	        }
+		}
+				
+	} // end TestGiftDeactivation
+}
---
+++ TestGiftOpenTotals.cs
@@ -0,0 +1,69 @@
+namespace Moneris
+{
+    using System;
+
+	public class TestGiftOpenTotals
+	{
+	  public static void Main(string[] args)
+	  {
+	        string host = "esqa.moneris.com";
+	        string store_id = args[0];
+	        string api_token = args[1];
+	        string ecr_no = args[2];
+	
+	        ErnexHttpsPostRequest mpgReq =
+	            new ErnexHttpsPostRequest(host, store_id, api_token, 
+                    new ErnexOpentotals (ecr_no));
+	        try
+	        {
+	            ErnexReceipt receipt = mpgReq.GetReceipt();
+                Console.WriteLine("ReceiptId = " + receipt.GetReceiptId());
+
+                if ( (receipt.GetReceiptId()).Equals("Global Error Receipt") ) 
+	            {
+                    Console.WriteLine("TransType = " + receipt.GetTransType());
+	    		    Console.WriteLine("ResponseCode = " + receipt.GetResponseCode());
+	    		    Console.WriteLine("Message = " + receipt.GetMessage());
+	    		    Console.WriteLine("Complete = " + receipt.GetComplete());
+	    		    Console.WriteLine("TransDate = " + receipt.GetTransDate());
+	    		    Console.WriteLine("TransTime = " + receipt.GetTransTime());
+	    		    Console.WriteLine("TimedOut = " + receipt.GetTimedOut());
+	            }
+	            else
+	            {
+                    foreach (string term_id in receipt.GetTerminalIDs())
+                    {
+                        Console.WriteLine("Terminal = " + term_id);
+                        Console.WriteLine("Closed = " + receipt.GetClosed(term_id) + "\n");
+
+                        foreach (string cardCode in receipt.GetEcrCardCodes(term_id))
+                        {
+                            Console.WriteLine("CardCode = " + cardCode);
+                            Console.WriteLine("PurchaseCount = " + receipt.GetPurchaseCount(term_id, cardCode));
+                            Console.WriteLine("PurchaseTotal = " + receipt.GetPurchaseTotal(term_id, cardCode));
+                            Console.WriteLine("PurchaseBenefitTotal = " + receipt.GetPurchaseBenefitTotal(term_id, cardCode));
+                            Console.WriteLine("RefundCount = " + receipt.GetRefundCount(term_id, cardCode));
+                            Console.WriteLine("RefundTotal = " + receipt.GetRefundTotal(term_id, cardCode));
+                            Console.WriteLine("RefundBenefitTotal = " + receipt.GetRefundBenefitTotal(term_id, cardCode));
+                            Console.WriteLine("RedemptionCount = " + receipt.GetRedemptionCount(term_id, cardCode));
+                            Console.WriteLine("RedemptionTotal = " + receipt.GetRedemptionTotal(term_id, cardCode));
+                            Console.WriteLine("RedemptionBenefitTotal = " + receipt.GetRedemptionBenefitTotal(term_id, cardCode));
+                            Console.WriteLine("ActivationCount = " + receipt.GetActivationCount(term_id, cardCode));
+                            Console.WriteLine("ActivationTotal = " + receipt.GetActivationTotal(term_id, cardCode));
+                            Console.WriteLine("CorrectionCount = " + receipt.GetCorrectionCount(term_id, cardCode));
+                            Console.WriteLine("CorrectionTotal = " + receipt.GetCorrectionTotal(term_id, cardCode));
+                            Console.WriteLine("Error Code = " + receipt.GetErrorCode());
+                            Console.WriteLine("Error Message = " + receipt.GetErrorMessage() + "\n");
+                        }
+                    }
+		       
+	            }
+	        }
+	        catch (Exception e)
+	        {
+	            Console.WriteLine(e);
+	        }
+		}
+				
+	} // end TestGiftOpenTotals
+}
